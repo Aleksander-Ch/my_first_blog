0ad54b7969f3a7fae538ea7685b3469f35aaa360
Mark Sheider <iliotropion@mail.ru>
2021-03-20 21:37:43 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,52 @@
+<?php
+
+// Объявляем переменную. Мы ее не можем менять но можем использовать в коде
+const BOO = 'Vasia Pupkin';
+
+// Примеры работы с строкой
+$name = 'Alex';
+$message = "Hello $name";
+
+$new_message = str_replace($name, 'Kris', $message);
+
+echo strlen($new_message) . PHP_EOL;
+
+// Примеры работы с числами
+
+$debit = 100;
+$kredit = 200;
+$something = $kredit/$debit;
+
+echo $something;
+
+if ($something == 2){
+    echo "Выражение верно";
+}else {echo "утверждение не верно " . PHP_EOL ;}
+
+/// Работа с утверждениями
+$is_Windows = true;
+
+if ($is_Windows) {
+    echo "Удали ее нафиг!!!" .  PHP_EOL;
+} else {
+    echo "Отлично! Так держать!";
+}
+
+echo PHP_EOL . BOO;
+
+class CardEnum {
+    const DEBIT = '';
+    const CREDIT = '';
+}
+
+
+CardEnum::CREDIT;
+
+
+
+
+// ----------------------------------------------------------
+
+// Массивы
+
+// Классы
